56132fd99c913696e052f49ecdc0a6f04a697a1a
pchator209 <pchator209@gmail.com>
2026-05-04 08:21:09 +0200

    lenght shelley

--- js/obres.js
+++ js/obres.js
@@ -1,8 +1,7 @@
 $(function () {
   var isCollapsed = false;
 
-  
-  var total = $("dl dd").size();
+  var total = $("dl dd").length;
 
   $("#infoRessenyes").text("Total de ressenyes: " + total);
 
